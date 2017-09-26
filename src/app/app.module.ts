6d1027ab415565ad2bf0a5824c6a78d32ac11415
Kyoto Kopz <kopz9999@gmail.com>
2017-09-26 11:59:01 -0700

    setup material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from "@angular/http";
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MaterialModule } from '@angular/material';
+import { MdButtonModule, MdCardModule, MdMenuModule,
+  MdToolbarModule, MdIconModule } from '@angular/material';
 
+import 'hammerjs';
 import { routes } from './app.routes';
 
 @NgModule({
@@ -15,6 +21,13 @@ import { routes } from './app.routes';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule, // Add material components to imports array
+    MdCardModule,
+    MdMenuModule,
+    MdToolbarModule,
+    MdIconModule,
     routes,
   ],
   providers: [],
